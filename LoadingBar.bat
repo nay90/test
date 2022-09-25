9770889ee448c9c78bb19d6b2682bcc43a63f90b
nay90 <78679776+nay90@users.noreply.github.com>
2022-09-25 21:56:27 +1000

    Add files via upload

---
+++ LoadingBar.bat
@@ -0,0 +1,25 @@
+@echo off
+title Loading
+:loading
+color 0a
+echo Loading
+ping localhost -n 2 >nul
+cls
+echo Loading.
+ping localhost -n 2 >nul
+cls
+echo Loading..
+ping localhost -n 2 >nul
+cls
+echo Loading...
+ping localhost -n 2 >nul
+cls
+echo Loading....
+ping localhost -n 2 >nul
+cls
+echo Loading.....
+ping localhost -n 2 >nul
+cls
+
+pause
+
